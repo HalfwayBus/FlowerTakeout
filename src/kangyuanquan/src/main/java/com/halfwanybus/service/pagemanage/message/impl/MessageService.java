9aadde229ed8fbd2100256a1301cd5b6f7c64813
LiangZhilin <891004865@qq.com>
2016-11-30 19:58:23 +0800

    留言管理完善

--- src/kangyuanquan/src/main/java/com/halfwanybus/service/pagemanage/message/impl/MessageService.java
+++ src/kangyuanquan/src/main/java/com/halfwanybus/service/pagemanage/message/impl/MessageService.java
@@ -20,6 +20,69 @@ import com.halfwanybus.service.pagemanage.message.MessageManager;
 @Service("messageService")
 public class MessageService implements MessageManager{
 
+	@Resource(name = "daoSupport")
+	private DaoSupport dao;
+	
+	/**新增
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void save(PageData pd)throws Exception{
+		dao.save("MessageMapper.save", pd);
+	}
+	
+	/**删除
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void delete(PageData pd)throws Exception{
+		dao.delete("MessageMapper.delete", pd);
+	}
+	
+	/**修改
+	 * @param pd
+	 * @throws Exception
+	 */
+	public void edit(PageData pd)throws Exception{
+		dao.update("MessageMapper.edit", pd);
+	}
+	
+	/**列表
+	 * @param page
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> list(Page page)throws Exception{
+		return (List<PageData>)dao.findForList("MessageMapper.datalistPage", page);
+	}
+	
+	/**列表(全部)
+	 * @param pd
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public List<PageData> listAll(PageData pd)throws Exception{
+		return (List<PageData>)dao.findForList("MessageMapper.listAll", pd);
+	}
+	
+	/**通过id获取数据
+	 * @param pd
+	 * @throws Exception
+	 */
+	public PageData findById(PageData pd)throws Exception{
+		return (PageData)dao.findForObject("MessageMapper.findById", pd);
+	}
+	
+	/**批量删除
+	 * @param ArrayDATA_IDS
+	 * @throws Exception
+	 */
+	public void deleteAll(String[] ArrayDATA_IDS)throws Exception{
+		dao.delete("MessageMapper.deleteAll", ArrayDATA_IDS);
+	}
+    
 
+	
+	
 }
 
